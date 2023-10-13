017beb8cde97364b5e3a6523c1c06107d503b653
Gabriel Maziero <gabrielmaziero@gmail.com>
2023-10-13 14:29:53 -0300

    fix headers

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -17,7 +17,7 @@ export const getRandomUserAgent = () => {
   return userAgents[Math.floor(Math.random() * userAgents.length)];
 };
 
-export const getHeaders = () => {
+export const getHeaders = (cookie?: string) => {
   const HEADERS = {
     accept:
       "*/*, text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8",
@@ -26,6 +26,7 @@ export const getHeaders = () => {
     DNT: "1",
     "User-Agent":
       "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299",
+      Cookie: cookie ?? "",
   };
 
   return HEADERS;

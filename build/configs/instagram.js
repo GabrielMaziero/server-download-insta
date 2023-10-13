249b79f090e6d37ad922a4d7500151eb5be80d83
Gabriel Maziero <gabrielmaziero@gmail.com>
2023-10-13 12:13:53 -0300

    remove folder build

--- build/configs/instagram.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var _a;
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authCookie = void 0;
-exports.authCookie = (_a = process.env.AUTH_COOKIE) !== null && _a !== void 0 ? _a : "";

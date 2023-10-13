3028fdece2123226ec5a507c59f0666aeb9641ce
Gabriel Maziero <gabrielmaziero@gmail.com>
2023-10-13 14:36:13 -0300

    return headers

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -19,14 +19,12 @@ export const getRandomUserAgent = () => {
 
 export const getHeaders = (cookie?: string) => {
   const HEADERS = {
-    accept:
-      "*/*, text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8",
-    host: "www.instagram.com",
-    referer: "https://www.instagram.com/",
-    DNT: "1",
-    "User-Agent":
-      "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36 Edge/16.16299",
-      Cookie: cookie ?? "",
+    "User-Agent": getRandomUserAgent(),
+    Cookie: cookie ?? "",
+    Accept: "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "Accept-Language": "en-us,en;q=0.5",
+    "Sec-Fetch-Mode": "navigate",
+    Referer: "https://www.instagram.com/",
   };
 
   return HEADERS;
